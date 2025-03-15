3cb0757982e82e4977cce36f9fb5dcfb7ca30964
pbobinsk <pbobinsk@gmail.com>
2025-03-15 11:25:06 +0100

    Zmiany na wykładzie 2.1

--- src/wsb/po/start/First.java
+++ src/wsb/po/start/First.java
@@ -2,6 +2,8 @@ package wsb.po.start;
 
 public class First {
     public static void main(String[] args) {
+
         System.out.println("Hello from first class.");
+        System.out.println("Coś jeszcze");
     }
 }
